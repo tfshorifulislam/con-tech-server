081f2b5d2538321954f83fb641f1727a11dd9a69
tfshorifulislam <tfshorifulislam@gmail.com>
2026-05-24 00:15:23 +0600

    setup done

--- index.js
+++ index.js
@@ -1,11 +1,59 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+const dns = require("node:dns");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const dotenv = require('dotenv')
+
+dotenv.config()
+app.use(cors())
+app.use(express.json())
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT
+const uri = process.env.MONGODB_URI;
+
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
+
+
+async function run() {
+    try {
+        await client.connect();
+
+        const db = client.db('contra-server')
+        const collectionPosts = db.collection('posts')
+
+
+
+
+
+
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${port}`);
 });
